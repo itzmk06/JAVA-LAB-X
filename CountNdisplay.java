cfb4c8478897f711e7a2874486f9dbff88d1ee00
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2023-12-05 20:56:05 +0530

    Update CountNdisplay.java

--- CountNdisplay.java
+++ CountNdisplay.java
@@ -1,3 +1,7 @@
+// program to count and display number of args
+// provide input by manually running 
+// ex : javac CountNdisplay.java
+// java CountNdisplay 1 2 3 4 5 6 
 public class CountNdisplay {
     public static void main(String[] args){
         int size=args.length;
